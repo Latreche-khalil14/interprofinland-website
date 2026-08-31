04a2fed24557df41e4784b08e60f7c31dce73924
InterProFinland Dev <dev@interprofinland.fi>
2026-08-31 18:13:59 +0100

    fix: Add missing env.d.ts, astro config and package files for Vercel build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.interprofinland.fi',
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: { enabled: false },
+  }),
   trailingSlash: 'never',
   build: {
     format: 'directory',
