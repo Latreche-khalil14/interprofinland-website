52b7b4f6288ffcbd2fddd6b1892c30307d098ceb
InterProFinland Dev <dev@interprofinland.fi>
2026-09-03 12:26:49 +0100

    feat: production improvements - remove console.logs, add sitemap, add Google Analytics support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +21,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
   vite: {
     resolve: {
